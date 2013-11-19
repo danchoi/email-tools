e82642c41f074c7479d76d8bfbc10f425e2436fb
Daniel Choi <dhchoi@gmail.com>
2013-11-19 15:19:04 -0500

    tool to extract html text part

---
+++ htmlpart.hs
@@ -0,0 +1,21 @@
+module Main where
+import Codec.MIME.Parse
+import Codec.MIME.Type
+import Data.List
+import Data.Maybe
+import System.IO
+import Text.Printf
+
+
+findPart :: MIMEValue -> String
+findPart m@(MIMEValue t@(Type (Text "html") _) d (Single c) h inc) = c
+-- attachments
+findPart (MIMEValue t@(Type _ mparams) d (Single c) h inc) = ""
+findPart (MIMEValue t@(Type (Multipart _) _) d (Multi cs) h inc) = concat $ map findPart cs
+findPart m = error $ "undefined: " ++ show m
+
+main = do
+  raw <- getContents
+  let m = parseMIMEMessage raw
+  let res = findPart m
+  putStrLn res
